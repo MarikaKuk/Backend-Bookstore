d04e928f8e672014c056750e34134ae1c8497fe5
MarikaKuk <a1501042@myy.haaga-helia.fi>
2026-02-01 19:59:56 +0200

    Chapter 3

--- bookstore/src/main/java/backend/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/backend/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package backend.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import backend.bookstore.domain.Book;
+import backend.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository bookRepository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			bookRepository.save(new Book("Kasvoton kuolema", "Henning Mankell", "123456-22", 45.60, 1995));
+			bookRepository.save(new Book("A Farewell to Arms", "Ernest Hemingway", "112233-55", 81.00, 1929));
+			bookRepository.save(new Book("Animal Farm", "George Orwell", "2212343-5", 75.00, 1945));
+
+			log.info("fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+
+	}
 }
--- bookstore/src/main/java/backend/bookstore/web/BookController.java
+++ bookstore/src/main/java/backend/bookstore/web/BookController.java
@@ -1,17 +1,58 @@
 package backend.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import backend.bookstore.domain.Book;
+import backend.bookstore.domain.BookRepository;
 
 @Controller
 
 public class BookController {
 
-    @GetMapping("/index")
-    @ResponseBody
-    public String greeting() {
-        return "hello";
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
+
+    @RequestMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        bookRepository.deleteById(id);
+        return "redirect:../booklist";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("book", bookRepository.findById(id));
+        return "editbook";
+    }
+
+    @PostMapping("edit/saveEdit")
+    public String saveBookEdit(Book book) {
+        bookRepository.save(book);
+        return "redirect:../booklist";
+    }
 }
